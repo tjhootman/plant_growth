a608493dfe43abccfb1b5b9bb8243bb3eeac0f65
tjhootman <tjhootman@gmail.com>
2025-04-24 20:58:59 -0400

    Add plotting of graphs for 3 plant types

--- plant_growth.py
+++ plant_growth.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 days = [1, 2, 3, 4, 5, 6, 7]
 
@@ -7,3 +7,11 @@ growth_type_a = [0.5, 1.2, 2.1, 3.0, 3.8, 4.5, 5.1]
 growth_type_b = [0.3, 0.8, 1.5, 2.2, 2.9, 3.5, 4.0]
 
 growth_type_c = [0.2, 0.5, 1.1, 1.8, 2.5, 3.1, 3.7]
+
+fig, ax = plt.subplots()
+
+ax.plot(days, growth_type_a, linewidth=3)
+ax.plot(days, growth_type_b, linewidth=3)
+ax.plot(days, growth_type_c, linewidth=3)
+
+plt.show()
