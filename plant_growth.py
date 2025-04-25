c6384445ba8231b7057c6bbdd0190775c3daaf4c
tjhootman <tjhootman@gmail.com>
2025-04-24 21:11:11 -0400

    Add labels, markers, grid, and legend

--- plant_growth.py
+++ plant_growth.py
@@ -10,12 +10,16 @@ growth_type_c = [0.2, 0.5, 1.1, 1.8, 2.5, 3.1, 3.7]
 
 fig, ax = plt.subplots()
 
-ax.plot(days, growth_type_a, linewidth=3)
-ax.plot(days, growth_type_b, linewidth=3)
-ax.plot(days, growth_type_c, linewidth=3)
+ax.plot(days, growth_type_a, label='Type A', marker='o', markersize=4)
+ax.plot(days, growth_type_b, label='Type B', marker='o', markersize=4)
+ax.plot(days, growth_type_c, label='Type C', marker='o', markersize=4)
 
 ax.set_title('Growth of Bean Seed Types Over One Week')
 ax.set_xlabel('Day')
 ax.set_ylabel('Height (cm)')
+
+ax.grid(True)
+
+ax.legend()
 
 plt.show()
