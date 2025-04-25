d0f6c8bd114eec9d3fa0e45848008685c7fb9e69
tjhootman <tjhootman@gmail.com>
2025-04-24 20:51:55 -0400

    Initial setup of plant growth data and files

---
+++ plant_growth.py
@@ -0,0 +1,9 @@
+import matplotlib as plt
+
+days = [1, 2, 3, 4, 5, 6, 7]
+
+growth_type_a = [0.5, 1.2, 2.1, 3.0, 3.8, 4.5, 5.1]
+
+growth_type_b = [0.3, 0.8, 1.5, 2.2, 2.9, 3.5, 4.0]
+
+growth_type_c = [0.2, 0.5, 1.1, 1.8, 2.5, 3.1, 3.7]
